189f67d9fd7c623bb8328e6e6205fd94ab87541c
tiago <tyagolp@gmail.com>
2021-08-17 23:24:21 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+
+import { CategoryRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoryRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category already exists" });
+    }
+
+    categoriesRepository.create({ name, description });
+
+    return response.status(201).send();
+    // return response.json(categories);
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+    return response.json(all);
+    // return response.json(categories);
+});
+
+export { categoriesRoutes };
