9ef5f31d80dd61be7203e9e43aa60b7e80f58cd6
Tiago <tyagolp@gmail.com>
2021-09-10 17:30:02 -0300

    add my first test TDD

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Criar categoria", () => {
+    it("Espero que 2 + 2 seja 4", () => {
+        const soma = 2 + 2;
+        const result = 4;
+
+        expect(soma).toBe(result);
+    });
+
+    it("Espero que 2 + 2 não seja 5", () => {
+        const soma = 2 + 2;
+        const result = 5;
+
+        expect(soma).not.toBe(result);
+    });
+});
