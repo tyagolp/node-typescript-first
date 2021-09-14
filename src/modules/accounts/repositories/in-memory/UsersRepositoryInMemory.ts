e10f6701354b439678382cd6f8e3b1a3891f0004
Tiago <tyagolp@gmail.com>
2021-09-14 18:22:30 -0300

    add unit test in AuthenticateUserUseCase

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+    users: User[] = [];
+
+    async create({
+        driver_license,
+        email,
+        password,
+        name,
+    }: ICreateUserDTO): Promise<void> {
+        const user = new User();
+
+        Object.assign(user, { driver_license, email, password, name });
+
+        this.users.push(user);
+    }
+
+    async findById(id: string): Promise<User> {
+        return this.users.find((user) => user.id === id);
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        return this.users.find((user) => user.email === email);
+    }
+}
+
+export { UsersRepositoryInMemory };
